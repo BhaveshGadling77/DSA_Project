462e846378bb108c4761e6cbe6d5f0071b7e332d
Piyush-D24 <piyushdeshpande100@gmail.com>
2025-10-17 13:25:37 +0530

    Events and venues structures added

---
+++ events.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct time {
+    short unsigned int hour;        // [0, 23]
+    short unsigned int minute;      // [0, 59]
+    short unsigned int second;      // [0, 59]
+} time;
+
+typedef struct date {
+    short int date;
+    short int month;
+    short int year
+} date;
+
+typedef struct event {
+    char eventName[24];
+    int eventID;
+    int guests;             // Number of guests
+    char organiser;
+    
+} event;
