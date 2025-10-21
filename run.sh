0af3c6f75717553c561a8ea686341386955897c0
BhaveshGadling77 <bhaveshgadling2025@gmail.com>
2025-10-21 09:47:18 +0530

    Updated the run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install -y libgtk-4-dev > /dev/null 2>&1
 
-gcc ./GUI/practice.c -o ./GUI/practice `pkg-config --cflags --libs gtk4`
+gcc ./GUI/some.c -o ./GUI/practice1 `pkg-config --cflags --libs gtk4`
 
-./GUI/practice
+./GUI/practice1
