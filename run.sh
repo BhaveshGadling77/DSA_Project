b6c1995a0b664ecaaaf22e583173af315b0ca8b8
BhaveshGadling77 <bhaveshgadling2025@gmail.com>
2025-10-30 00:33:56 +0530

    Update in Login Registration System.

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo apt-get install -y libgtk-4-dev > /dev/null 2>&1
-
-gcc ./GUI/practice.c -o ./GUI/practice1 `pkg-config --cflags --libs gtk4`
-
-./GUI/practice1
+cd ./client/
+npm install
+npm run dev
