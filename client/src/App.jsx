3545e4e9f0c6d4b30c262cad85e7ae466f73221d
SumitDesai-21 <sumit.desai3433@gmail.com>
2025-10-31 16:01:36 +0530

    Tried git pull origin main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
-import Footer from './Components/Footer'
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+
 const App = () => {
   return (
-    <div className='min-h-screen flex flex-col'>
-    <Navbar/>
+    <div>
+      <Navbar/>
       <Footer/>
     </div>
   )
