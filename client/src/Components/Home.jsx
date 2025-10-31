5aae981e2d3d31bb3f0b6c555a6b800d23a1293a
SumitDesai-21 <sumit.desai3433@gmail.com>
2025-10-31 17:02:34 +0530

    Added components & their Routes

---
+++ client/src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen flex flex-col'>
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
