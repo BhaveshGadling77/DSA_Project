fe469d4a95e6c81c500cf47ab0104a04f819baaa
BhaveshGadling77 <bhaveshgadling2025@gmail.com>
2025-11-18 01:29:17 +0530

    Done the unique id generation.

--- Login_Registration_system/Organiser.h
+++ Login_Registration_system/Organiser.h
@@ -2,10 +2,17 @@ typedef struct Organiser {
     int organiserID;
     char name[64];
     unsigned int noOfEventsOrganised; /*No. of events organised.*/
-    unsigned long long mobileNumber;
-    char email[64];
-    char organisationName[64];
+    long long mobileNumber;
+    char email[128];
 } Organiser;
+
+typedef struct user {
+	int userId;
+	char name[64];
+	unsigned int noOfEventsAttended;
+	long long mobileNumber;
+	char email[128];
+} user;
 
 void RegisterAsOrganiser();
 
