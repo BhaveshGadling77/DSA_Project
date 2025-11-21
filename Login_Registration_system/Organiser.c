104733bc63aa7cff36a847489514a81da6da4ea1
SumitDesai-21 <sumit.desai3433@gmail.com>
2025-11-21 09:42:19 +0530

    modified code

--- Login_Registration_system/Organiser.c
+++ Login_Registration_system/Organiser.c
@@ -10,8 +10,7 @@ typedef struct Organiser {
     char name[64];
     unsigned int noOfEventsOrganised; /*No. of events organised.*/
     long long mobileNumber;
-    long long mobileNumber;
-    char email[64];
+    char email[128];
 } Organiser;
 
 typedef struct user {
@@ -19,7 +18,7 @@ typedef struct user {
 	char name[64];
 	unsigned int noOfEventsAttended;
 	long long mobileNumber;
-	char email[64];
+	char email[128];
 } user;
 
 
@@ -160,9 +159,10 @@ void RegisterAsAttendee() {
 	chdir("../Data");
 	FILE *fp = fopen("./userAttendee.csv", "a");
 	if (fp == -1) {
-		printf("Error in Opening User File");
+		printf("Error in Opening User File\n");
 		exit(1);
 	}
 	fprintf(fp, "%d,%s,%ld,%u,%s\n", att->userId, att->name, att->mobileNumber, att->noOfEventsAttended, att->email);
 	fclose(fp);
 }
+
